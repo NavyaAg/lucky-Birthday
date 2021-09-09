32e54d68ebe971f7ec2d466f6028b6b6bfedf21c
NavyaAg <itsNavya001@gmail.com>
2021-09-09 21:12:34 +0530

    functionality for checking lucky bday added

--- index.js
+++ index.js
@@ -3,7 +3,25 @@ var number=document.querySelector(".lucky-no");
 var button=document.querySelector(".check");
 
 button.addEventListener("click",function(){
-    console.log(date.value);
-    console.log(number.value);
-    console.log("clicked");
+    var total=calculateSum(date.value.replaceAll('-',''));
+    console.log(total)
+    if(total%number.value==0){
+        document.querySelector(".message").innerText="lucky bday";
+    }
+    else{
+        document.querySelector(".message").innerText="unlucky bday";
+    }   
 });
+
+function calculateSum(no){
+    var sum=0,dig;
+    console.log(no);
+    for(i=no;i>0;){   
+         dig=i%10;
+        sum+=dig;
+        i=Math.trunc(i/10)
+    }
+    return sum;
+}
+
+/*validate both inputs -no is filled nd year is correct */
